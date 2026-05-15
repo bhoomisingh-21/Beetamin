ea7e386eb94ade138c1943b16950cdd6e22adf5e
Bhoomi Singh <bhoomisingh2109@gmail.com>
2026-05-15 14:06:22 +0530

    added meta correctly

--- app/layout.tsx
+++ app/layout.tsx
@@ -103,7 +103,7 @@ export const metadata: Metadata = {
   manifest: '/site.webmanifest',
 
   verification: {
-    google: 'vocZ7ntJQnQy1wZGHbcNnCmxCMPrv-CchCUruZsYP_Q>',
+    google: 'vocZ7ntJQnQy1wZGHbcNnCmxCMPrv-CchCUruZsYP_Q',
   },
 }
 
